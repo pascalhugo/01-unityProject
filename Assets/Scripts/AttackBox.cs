b7ccd9bb38e45bc1324bc56d295e1b094536dbc9
Hugo PASCAL <pascal.hugo.16@gmail.com>
2016-05-24 12:52:11 +0200

    Init project

---
+++ Assets/Scripts/AttackBox.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackBox : MonoBehaviour {
+
+	public Fighter fighter;
+	public Collider2D collider;
+
+	public string AttackerTag;
+	public string TargetTag;
+
+	// private string tag;
+
+	void Start() {
+		fighter = GetComponentInParent<Fighter> ();
+		collider = GetComponentInParent<Collider2D> ();
+		Debug.Log (collider.tag);
+		// Debug.Log (GameObject.tag);
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (AttackerTag == "Enemy") {
+			if (col.isTrigger != true && (col.CompareTag ("Ally") || col.CompareTag ("Player"))) {
+				col.SendMessageUpwards ("GetHit", fighter);
+			}
+		} else if (AttackerTag == "Ally") {
+			if (col.isTrigger != true && col.CompareTag ("Enemy")) {
+				col.SendMessageUpwards ("GetHit", fighter);
+			}
+		}
+	}
+	
+
+}
